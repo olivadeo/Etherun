9148f7ccefa68d959cf077f67c7b9b372d12cd0a
olivadeo <34307316+olivadeo@users.noreply.github.com>
2024-04-04 12:50:42 +0200

    clean

--- backend/ignition/modules/RunningNFT.js
+++ backend/ignition/modules/RunningNFT.js
@@ -13,15 +13,7 @@ async function main() {
   const raceOrganizer = await hre.ethers.deployContract("RaceOrganizer");
   await raceOrganizer.waitForDeployment();
   console.log("RaceOrganizer address : "+ raceOrganizer.target);
-/*
-  try {
-    const tx = await raceOrganizer.registerOrganizerBackDoorforFree("0x70997970C51812dc3A010C7d01b50e0d17dc79C8","oliv");
-    await tx.wait();
-    console.log('Transaction successful');
-  } catch (error) {
-    console.error('Transaction failed:', error);
-  }
-*/
+
   const Runners = await ethers.getContractFactory("Runners");
   const runners = await Runners.deploy(medicNFT.target); 
 
@@ -42,14 +34,7 @@ async function main() {
 
   await medicNFT.authorizeContract(runners.target);
   await race.authorizeContract(market.target);
-  await race.setTicketMarket(market.target);
-
-
-/*
-ADMIN #0: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 (10000 ETH)
-ORGANIZER #1: 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 (10000 ETH)
-RUNNER #2: 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC (10000 ETH)
-*/
+  await race.setTicketMarket(market.target)
 
 
 }
